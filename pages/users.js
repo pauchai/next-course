a90cc939a28cef7c1a3082931ce09dae2e0e841c
Pauk <paul.khimyack@gmail.com>
2022-01-25 15:11:16 +0200

    users list

--- pages/users.js
+++ pages/users.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 const Users = () => {
+    const [users, setUsers] = useState([
+        {id: 1, name: 'user1'},
+        {id: 2, name: 'user2'},
+    ])
   return (
   <div>
-      Users list
+      <h1>Users list</h1>
+      <ul>
+          {users.map(user =>
+            <li>{user.name}</li>
+        )}
+      </ul>
   </div>
   )
 };
