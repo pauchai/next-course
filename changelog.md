75866e8a6a0c496c74e8b5749f6e8b34fd3a5166
Pauk <paul.khimyack@gmail.com>
2022-01-25 16:10:19 +0200

    Fetching data from server as usualy in react

--- changelog.md
+++ changelog.md
@@ -10,3 +10,6 @@ http://nextjs.org/docs/basic-features/built-in-css-suppurt
 Component individual css
 
 npm i scss
+
+json-server --watch db.json --port 5000
+Fetching data from server
--- pages/users.js
+++ pages/users.js
@@ -1,11 +1,17 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Users = () => {
     const [users, setUsers] = useState([
         {id: 1, name: 'user1'},
         {id: 2, name: 'user2'},
     ])
+
+    useEffect(async () => {
+        const response = await fetch(`http://localhost:5000/users`)
+        const data = await response.json()
+        setUsers(data)
+    }, [])
   return (
   <div>
       <h1>Users list</h1>
