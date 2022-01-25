479df672d594f70723856e56aa2e08afaf74e181
Pauk <paul.khimyack@gmail.com>
2022-01-25 15:01:00 +0200

    navigation1

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 const index = () => {
   return (
     <div>
+        <div>
+            <a href="/">Main</a>
+            <a href="/users">Users</a>
+        </div>
         <h1>Main page</h1>
     </div>
   )
 };
 
 export default index;
+
+<a href="">Main</a>
